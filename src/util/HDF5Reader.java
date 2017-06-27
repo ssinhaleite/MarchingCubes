b5798940713a4d7efad45319dcd3e5b92e1fdceb
Vanessa Leite <vrcleite@gmail.com>
2017-06-27 11:29:19 +0200

    Format code

--- src/util/HDF5Reader.java
+++ src/util/HDF5Reader.java
@@ -7,28 +7,25 @@ import bdv.bigcat.ui.ModalGoldenAngleSaturatedARGBStream;
 import bdv.img.h5.H5LabelMultisetSetupImageLoader;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
 
-public class HDF5Reader
-{
+public class HDF5Reader {
+
 	protected static int setupId = 0;
 
-	final static protected int[] cellDimensions = new int[] { 4, 4, 40 };
+	final static protected int[] cellDimensions = new int[] { 64, 64, 8 };
 
 	/** color generator for composition of loaded segments and canvas */
 	protected static ModalGoldenAngleSaturatedARGBStream colorStream;
 
-	public static ArrayList< H5LabelMultisetSetupImageLoader > readLabels( final IHDF5Reader reader, final String labelDataset ) throws IOException
-	{
-		final ArrayList< H5LabelMultisetSetupImageLoader > labels = new ArrayList<>();
+	public static ArrayList<H5LabelMultisetSetupImageLoader> readLabels(final IHDF5Reader reader,
+			final String labelDataset) throws IOException {
 
-		/* labels */
-//		System.out.println( "readLabels starts" );
-		final H5LabelMultisetSetupImageLoader labelLoader = new H5LabelMultisetSetupImageLoader( reader, null,
-				labelDataset, setupId++, cellDimensions );
+		/** loaded segments */
+		final ArrayList<H5LabelMultisetSetupImageLoader> labels = new ArrayList<>();
 
-//		System.out.println( "labelLoader: " + labelLoader );
-		labels.add( labelLoader );
-		//		System.out.println( "readLabels ends" );
-		
+		final H5LabelMultisetSetupImageLoader labelLoader = new H5LabelMultisetSetupImageLoader(reader, null,
+				labelDataset, setupId++, cellDimensions);
+		labels.add(labelLoader);
+
 		return labels;
 	}
 
