e098997a3ee00eaed061b878d4eff7a01628a317
Vanessa Leite <vrcleite@gmail.com>
2017-07-21 11:20:40 +0200

    Clean the mesh: we are not using triangles anymore

--- src/viewer/Mesh.java
+++ src/viewer/Mesh.java
@@ -11,49 +11,27 @@ public class Mesh
 	int numberOfVertices;
 
 	/**
-	 * the vertices positions as an array of points 
+	 * the vertices positions as an array of points
 	 */
 	float[][] vertices;
-
-	/**
-	 * the number of faces
-	 */
-	public int numberOfTriangles;
-
-	/**
-	 * the triangles given by 3 vertex indices (length = numberOfTriangles * 3)
-	 */
-	int[] triangles;
 
 	Mesh( int vertexCount, float[][] verticesArray, int trianglesCount, int[] trianglesArray )
 	{
 		numberOfVertices = vertexCount;
 		vertices = verticesArray;
-		numberOfTriangles = trianglesCount;
-		triangles = trianglesArray;
 	}
 
 	public Mesh()
 	{}
-	
+
 	public int getNumberOfVertices()
 	{
 		return numberOfVertices;
 	}
-	
+
 	public void setNumberOfVertices( int nVertices )
 	{
 		numberOfVertices = nVertices;
-	}
-	
-	public int getNumberOfTriangles()
-	{
-		return numberOfTriangles;
-	}
-
-	public void setNumberOfTriangles( int ntriangles )
-	{
-		numberOfTriangles= ntriangles;
 	}
 
 	public float[][] getVertices()
@@ -61,18 +39,8 @@ public class Mesh
 		return vertices;
 	}
 
-	public void setVertices(float[][] verticesArray)
+	public void setVertices( float[][] verticesArray )
 	{
 		vertices = verticesArray;
-	}
-
-	public int[] getTriangles()
-	{
-		return triangles;
-	}
-	
-	public void setTriangles( int[] trianglesArray)
-	{
-		triangles = trianglesArray;
 	}
 }
