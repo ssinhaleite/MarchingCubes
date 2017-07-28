6f42ec07b6d8dd07c4be144b1bef75fe7e00cd58
Vanessa Leite <vrcleite@gmail.com>
2017-07-28 17:27:31 +0200

    Creates the first chunk and update the next() method
    Now, the creation of the chunks is generated by threads in the next() method.
    Still not working properly, the method adds many times the same position/chunk, this way, the list of future mesh in some moment returns zero, but in fact there are chunks to be processed.

--- src/viewer/MeshExtractor.java
+++ src/viewer/MeshExtractor.java
@@ -1,5 +1,6 @@
 package viewer;
 
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,13 +15,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import bdv.labels.labelset.LabelMultisetType;
+import graphics.scenery.Mesh;
 import net.imglib2.RandomAccessibleInterval;
 import util.Chunk;
 
 /**
  * This class is responsible for generate region of interests (map of
- * boundaries). Will call the VolumePartitioner must have a callNext method (use
- * chunk index based on x, y and z)
+ * boundaries).
+ * 
+ * Will call the VolumePartitioner must have a callNext method (use chunk index
+ * based on x, y and z)
  * 
  * @author vleite
  *
@@ -30,21 +34,31 @@ public class MeshExtractor
 	/** logger */
 	static final Logger LOGGER = LoggerFactory.getLogger( MeshExtractor.class );
 
-	private RandomAccessibleInterval< LabelMultisetType > volumeLabels;
+	static final int MAX_CUBE_SIZE = 32;
 
-	Map< Integer, Chunk > chunkMap = new HashMap< Integer, Chunk >();
+	private RandomAccessibleInterval< LabelMultisetType > volumeLabels;
 
 	int[] initialPosition;
 
 	int[] partitionSize;
 
-	int numberOfPartitions = 0;
-
-	private int[] cubeSize;;
+	private int[] cubeSize;
 
 	private int foregroundValue;
 
 	private marchingCubes.MarchingCubes.ForegroundCriterion criterion;
+
+	private static Map< Future< viewer.Mesh >, Chunk > resultMeshMap = null;
+
+	private static List< Future< Chunk > > resultChunkList = null;
+
+	private static CompletionService< viewer.Mesh > executor = null;
+
+	private static CompletionService< Chunk > chunkExecutor = null;
+
+	private static util.VolumePartitioner partitioner;
+
+	private static List< int[] > positionsExtracted;
 
 	public MeshExtractor( RandomAccessibleInterval< LabelMultisetType > volumeLabels, final int[] cubeSize, final int foregroundValue, final marchingCubes.MarchingCubes.ForegroundCriterion criterion )
 	{
@@ -54,171 +68,242 @@ public class MeshExtractor
 		this.foregroundValue = foregroundValue;
 		this.criterion = criterion;
 
-		for ( int i = 0; i < volumeLabels.numDimensions(); i++ )
-		{
-			this.initialPosition[ i ] = ( int ) volumeLabels.min( i );
-		}
-
-		generatePartitionSize();
-
-		createChunks();
-	}
-
-	private void createChunks()
-	{
-		List< Chunk > chunks = new ArrayList< Chunk >();
-
-		CompletionService< viewer.Mesh > executor = null;
-		List< Future< viewer.Mesh > > resultMeshList = null;
-
-		util.VolumePartitioner partitioner = new util.VolumePartitioner( volumeLabels, partitionSize, cubeSize );
-		chunks = partitioner.dataPartitioning();
-
-		LOGGER.info( "starting executor..." );
 		executor = new ExecutorCompletionService< viewer.Mesh >(
 				Executors.newWorkStealingPool() );
 
-		resultMeshList = new ArrayList<>();
+		chunkExecutor = new ExecutorCompletionService< Chunk >(
+				Executors.newWorkStealingPool() );
 
-		if ( LOGGER.isDebugEnabled() )
-		{
-			LOGGER.debug( "creating callables for " + chunks.size() + " partitions..." );
-		}
+		resultMeshMap = new HashMap< Future< viewer.Mesh >, Chunk >();
 
-		for ( int i = 0; i < chunks.size(); i++ )
-		{
-			int[] subvolDim = new int[] { ( int ) chunks.get( i ).getVolume().dimension( 0 ), ( int ) chunks.get( i ).getVolume().dimension( 1 ),
-					( int ) chunks.get( i ).getVolume().dimension( 2 ) };
+		generatePartitionSize();
 
-			MarchingCubesCallable callable = new MarchingCubesCallable( chunks.get( i ).getVolume(), subvolDim, chunks.get( i ).getOffset(), cubeSize, criterion, foregroundValue,
-					true );
-
-			if ( LOGGER.isDebugEnabled() )
-			{
-				LOGGER.debug( "dimension: " + chunks.get( i ).getVolume().dimension( 0 ) + "x" + chunks.get( i ).getVolume().dimension( 1 )
-						+ "x" + chunks.get( i ).getVolume().dimension( 2 ) );
-				LOGGER.debug( "offset: " + chunks.get( i ).getOffset()[ 0 ] + " " + chunks.get( i ).getOffset()[ 1 ] + " " + chunks.get( i ).getOffset()[ 2 ] );
-				LOGGER.debug( "callable: " + callable );
-			}
-
-			Future< viewer.Mesh > result = executor.submit( callable );
-			resultMeshList.add( result );
-		}
-
-		Future< viewer.Mesh > completedFuture = null;
-		LOGGER.info( "waiting results..." );
-
-		while ( resultMeshList.size() > 0 )
-		{
-			// block until a task completes
-			try
-			{
-				completedFuture = executor.take();
-				if ( LOGGER.isTraceEnabled() )
-				{
-					LOGGER.trace( "task " + completedFuture + " is ready: " + completedFuture.isDone() );
-				}
-			}
-			catch ( InterruptedException e )
-			{
-				// TODO Auto-generated catch block
-				LOGGER.error( " task interrupted: " + e.getCause() );
-			}
-
-			resultMeshList.remove( completedFuture );
-			viewer.Mesh m = new viewer.Mesh();
-
-			// get the mesh, if the task was able to create it
-			try
-			{
-				m = completedFuture.get();
-				LOGGER.info( "getting mesh" );
-			}
-			catch ( InterruptedException | ExecutionException e )
-			{
-				LOGGER.error( "Mesh creation failed: " + e.getCause() );
-				break;
-			}
-
-			// a mesh was created, so update the existing mesh
-			if ( m.getNumberOfVertices() > 0 )
-			{
-				LOGGER.info( "updating mesh..." );
-				if ( LOGGER.isDebugEnabled() )
-				{
-					LOGGER.debug( "size of mesh " + m.getNumberOfVertices() );
-				}
-
-				// TODO: need to call the viewer to update the mesh
-//				updateMesh( m, neuron, false );
-//				neuron.setVertices( FloatBuffer.wrap( verticesArray ) );
-//				neuron.recalculateNormals();
-//				neuron.setDirty( true );
-			}
-		}
+		partitioner = new util.VolumePartitioner( this.volumeLabels, partitionSize, this.cubeSize );
+		
+		positionsExtracted = new ArrayList< int[] >();
 	}
 
 	public boolean hasNext()
 	{
-		if ( numberOfPartitions == 0 )
-		{
-			return false;
-		}
-
-		return true;
+		System.out.println( "hasNext - there is/are " + resultMeshMap.size() + " threads to calculate the mesh" );
+		return resultMeshMap.size() > 0;
 	}
 
-	public viewer.Mesh next()
+	public Mesh next()
 	{
-		numberOfPartitions--;
-
-		Chunk chunk = new Chunk();
-
-		for ( int i = 0; i < volumeLabels.numDimensions(); i++ )
+		Future< viewer.Mesh > completedFuture = null;
+		// block until any task completes
+		try
 		{
-			initialPosition[ i ] += partitionSize[ i ];
+			completedFuture = executor.take();
+			if ( LOGGER.isTraceEnabled() )
+			{
+				LOGGER.trace( "task " + completedFuture + " is ready: " + completedFuture.isDone() );
+			}
+		}
+		catch ( InterruptedException e )
+		{
+			// TODO Auto-generated catch block
+			LOGGER.error( " task interrupted: " + e.getCause() );
 		}
 
-		CompletionService< viewer.Mesh > executor = null;
-		List< Future< viewer.Mesh > > resultMeshList = null;
+		Chunk chunk = resultMeshMap.remove( completedFuture );
+		viewer.Mesh m = new viewer.Mesh();
 
-		util.VolumePartitioner partitioner = new util.VolumePartitioner( volumeLabels, partitionSize, cubeSize );
-		chunk = partitioner.getChunk( initialPosition );
+		// get the mesh, if the task was able to create it
+		try
+		{
+			m = completedFuture.get();
+			LOGGER.info( "getting mesh" );
+		}
+		catch ( InterruptedException | ExecutionException e )
+		{
+			LOGGER.error( "Mesh creation failed: " + e.getCause() );
+		}
 		
-		chunkMap.put( numberOfPartitions, chunk );
+		Mesh sceneryMesh = new Mesh();
+		updateMesh( m, sceneryMesh );
 
-		LOGGER.info( "starting executor..." );
-		executor = new ExecutorCompletionService< viewer.Mesh >(
-				Executors.newWorkStealingPool() );
+		chunk.setMesh( sceneryMesh, cubeSize );
 
-		return new viewer.Mesh();
+		// if one of the neighbors positions exist, creates a new callable for
+		// them
+		Thread thread1 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x + partitionSizeX, y, z
+				int[] newPosition = new int[] { initialPosition[ 0 ] + partitionSize[ 0 ], initialPosition[ 1 ], initialPosition[ 2 ] };
+				if ( initialPosition[ 0 ] + partitionSize[ 0 ] < volumeLabels.dimension( 0 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread1.start();
+
+		Thread thread2 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x - partitionSizeX, y, z
+				int[] newPosition = new int[] { initialPosition[ 0 ] - partitionSize[ 0 ], initialPosition[ 1 ], initialPosition[ 2 ] };
+				if ( initialPosition[ 0 ] - partitionSize[ 0 ] > volumeLabels.min( 0 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread2.start();
+
+		Thread thread3 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x, y + partitionSizeY, z
+				int[] newPosition = new int[] { initialPosition[ 0 ], initialPosition[ 1 ] + partitionSize[ 1 ], initialPosition[ 2 ] };
+				if ( initialPosition[ 1 ] + partitionSize[ 1 ] < volumeLabels.dimension( 1 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread3.start();
+
+		Thread thread4 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x, y - partitionSizeY, z
+				int[] newPosition = new int[] { initialPosition[ 0 ], initialPosition[ 1 ] - partitionSize[ 1 ], initialPosition[ 2 ] };
+				if ( initialPosition[ 1 ] - partitionSize[ 1 ] > volumeLabels.min( 1 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread4.start();
+
+		Thread thread5 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x, y, z + partitionSizeZ
+				int[] newPosition = new int[] { initialPosition[ 0 ], initialPosition[ 1 ], initialPosition[ 2 ] + partitionSize[ 2 ] };
+				if ( initialPosition[ 2 ] + partitionSize[ 2 ] < volumeLabels.dimension( 2 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread5.start();
+
+		Thread thread6 = new Thread( new Runnable()
+		{
+			public void run()
+			{
+				// x, y, z - partitionSizeZ
+				int[] newPosition = new int[] { initialPosition[ 0 ], initialPosition[ 1 ], initialPosition[ 2 ] - partitionSize[ 2 ] };
+				if ( initialPosition[ 2 ] - partitionSize[ 2 ] > volumeLabels.min( 2 ) )
+					createChunks( newPosition );
+			}
+		} );
+		thread6.start();
+
+		// a mesh was created, return it
+		return sceneryMesh;
+	}
+
+	public void createChunks( int[] position )
+	{
+		initialPosition = position;
+
+		System.out.println( "there is/are " + resultMeshMap.size() + " threads to calculate the mesh" );
+		// creates the callable for the chunk in the given position
+		System.out.println( "createChunks - " + position[ 0 ] + " " + position[ 1 ] + " " + position[ 2 ] );
+
+		for ( int i = 0; i < positionsExtracted.size(); i++ )
+		{
+			int[] test = positionsExtracted.get( i );
+			if ( test[ 0 ] == position[ 0 ] && test[ 1 ] == position[ 1 ] && test[ 2 ] == position[ 2 ] )
+				return;
+		}
+
+		positionsExtracted.add( position );
+
+		System.out.println( "adding in the set: " + position[ 0 ] + " " + position[ 1 ] + " " + position[ 2 ] );
+		
+		Chunk chunk = partitioner.getChunk( position );
+		createCallable( chunk );
+	}
+
+	private void createCallable( Chunk chunk )
+	{
+		// if the chunk is already in the callable, do not add it again
+		if ( resultMeshMap.containsValue( chunk ) ) { return; }
+
+		int[] volumeDimension = new int[] { ( int ) chunk.getVolume().dimension( 0 ), ( int ) chunk.getVolume().dimension( 1 ),
+				( int ) chunk.getVolume().dimension( 2 ) };
+
+		MarchingCubesCallable callable = new MarchingCubesCallable( chunk.getVolume(), volumeDimension, chunk.getOffset(), cubeSize, criterion, foregroundValue,
+				true );
+
+		if ( LOGGER.isDebugEnabled() )
+		{
+			LOGGER.debug( "dimension: " + volumeDimension[ 0 ] + "x" + volumeDimension[ 1 ] + "x" + volumeDimension[ 2 ] );
+			LOGGER.debug( "offset: " + chunk.getOffset()[ 0 ] + " " + chunk.getOffset()[ 1 ] + " " + chunk.getOffset()[ 2 ] );
+		}
+
+		Future< viewer.Mesh > result = executor.submit( callable );
+		resultMeshMap.put( result, chunk );
 	}
 
 	private void generatePartitionSize()
 	{
-		int numberOfCellsX = ( int ) ( ( volumeLabels.max( 0 ) - volumeLabels.min( 0 ) ) + 1 ) / 32;
-		int numberOfCellsY = ( int ) ( ( volumeLabels.max( 1 ) - volumeLabels.min( 1 ) ) + 1 ) / 32;
-		int numberOfCellsZ = ( int ) ( ( volumeLabels.max( 2 ) - volumeLabels.min( 2 ) ) + 1 ) / 32;
+		for ( int i = 0; i < partitionSize.length; i++ )
+		{
+			partitionSize[ i ] = ( int ) ( ( volumeLabels.max( i ) - volumeLabels.min( i ) ) + 1 ) / MAX_CUBE_SIZE;
+		}
 
-		LOGGER.trace( "division: " + numberOfCellsX + " " + numberOfCellsY + " " + numberOfCellsZ );
+		LOGGER.trace( "division: {}, {}, {}", partitionSize[ 0 ], partitionSize[ 1 ], partitionSize[ 2 ] );
 
-		numberOfCellsX = numberOfCellsX >= 7 ? 7 * 32 : numberOfCellsX * 32;
-		numberOfCellsY = numberOfCellsY >= 7 ? 7 * 32 : numberOfCellsY * 32;
-		numberOfCellsZ = numberOfCellsZ >= 7 ? 7 * 32 : numberOfCellsZ * 32;
+		for ( int i = 0; i < partitionSize.length; i++ )
+		{
+			partitionSize[ i ] = partitionSize[ i ] >= 7 ? 7 * MAX_CUBE_SIZE : partitionSize[ i ] * MAX_CUBE_SIZE;
+		}
 
-		LOGGER.trace( "partition size 1: " + numberOfCellsX + " " + numberOfCellsY + " " + numberOfCellsZ );
+		LOGGER.trace( "partition size: {}, {}, {}", partitionSize[ 0 ], partitionSize[ 1 ], partitionSize[ 2 ] );
 
-		numberOfCellsX = ( numberOfCellsX == 0 ) ? 1 : numberOfCellsX;
-		numberOfCellsY = ( numberOfCellsY == 0 ) ? 1 : numberOfCellsY;
-		numberOfCellsZ = ( numberOfCellsZ == 0 ) ? 1 : numberOfCellsZ;
+		for ( int i = 0; i < partitionSize.length; i++ )
+		{
+			partitionSize[ i ] = ( partitionSize[ i ] == 0 ) ? 1 : partitionSize[ i ];
+		}
 
-		LOGGER.trace( "zero verification: " + numberOfCellsX + " " + numberOfCellsY + " " + numberOfCellsZ );
+		LOGGER.trace( "final partition size: {}, {}, {}", partitionSize[ 0 ], partitionSize[ 1 ], partitionSize[ 2 ] );
+	}
 
-		partitionSize = new int[] { numberOfCellsX, numberOfCellsY, numberOfCellsZ };
-		LOGGER.trace( "final partition size: " + numberOfCellsX + " " + numberOfCellsY + " " + numberOfCellsZ );
+	/**
+	 * this method convert the viewer mesh into the scenery mesh
+	 * 
+	 * @param mesh
+	 *            mesh information to be converted in a mesh for scenery
+	 * @param sceneryMesh
+	 *            scenery mesh that will receive the information
+	 */
+	public void updateMesh( viewer.Mesh mesh, Mesh sceneryMesh )
+	{
+		float[] verticesArray = new float[ mesh.getNumberOfVertices() * 3 ];
 
-		numberOfPartitions = ( int ) ( ( ( volumeLabels.max( 0 ) - volumeLabels.min( 0 ) ) + 1 ) / numberOfCellsX ) *
-				( int ) ( ( ( ( volumeLabels.max( 1 ) - volumeLabels.min( 1 ) ) + 1 ) / numberOfCellsY ) ) *
-				( int ) ( ( ( ( volumeLabels.max( 2 ) - volumeLabels.min( 2 ) ) + 1 ) / numberOfCellsZ ) - 1 );
+		float[][] vertices = mesh.getVertices();
+		int v = 0;
+		for ( int i = 0; i < mesh.getNumberOfVertices(); i++ )
+		{
+			verticesArray[ v++ ] = vertices[ i ][ 0 ];
+			verticesArray[ v++ ] = vertices[ i ][ 1 ];
+			verticesArray[ v++ ] = vertices[ i ][ 2 ];
+		}
+
+		final float maxX = volumeLabels.dimension( 0 ) - 1;
+		final float maxY = volumeLabels.dimension( 1 ) - 1;
+		final float maxZ = volumeLabels.dimension( 2 ) - 1;
+
+		float maxAxisVal = Math.max( maxX, Math.max( maxY, maxZ ) );
+		// omp parallel for
+		for ( int i = 0; i < verticesArray.length; i++ )
+		{
+			verticesArray[ i ] /= maxAxisVal;
+		}
+
+		sceneryMesh.setVertices( FloatBuffer.wrap( verticesArray ) );
 	}
 }
