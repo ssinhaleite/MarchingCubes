561544b4cf3d17ababf2f1563ca7a6a8843e4ac8
Vanessa Leite <vrcleite@gmail.com>
2017-08-07 12:43:33 +0200

    Add default parameters

--- src/util/Parameters.java
+++ src/util/Parameters.java
@@ -11,13 +11,15 @@ import com.beust.jcommander.Parameter;
 public class Parameters
 {
 	@Parameter( names = { "--file", "-f" }, description = "input file path (hdf5)" )
-	public String filePath = "";
+	public String filePath = "resources/sample_B_20160708_frags_46_50.hdf";
+//	public String filePath = "data/sample_B.augmented.0.hdf";
 
 	@Parameter( names = { "--label", "-l" }, description = "label dataset name" )
 	public String labelDatasetPath = "/volumes/labels/neuron_ids";
 
 	@Parameter( names = { "--neuronId", "-id" }, description = "foreground value" )
-	public Integer foregroundValue = 1;
+	public Integer foregroundValue = 7;
+//	public Integer foregroundValue = 1854;
 
 	@Parameter( names = "--help", help = true )
 	private boolean help;
